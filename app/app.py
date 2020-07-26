6f86283b41fca00ca0d71b1e3bcc0d02e80a8e10
gastonnrodriguez <gastonnrodriguez@gmail.com>
2020-07-26 11:40:50 -0300

    cargando modelo keras para analisis de ingles

--- app/app.py
+++ app/app.py
@@ -10,29 +10,44 @@ app = flask.Flask(__name__)
 model = None
 
 
-def load_trained_model_():
+def load_trained_model():
     print("Cargando modelo..")
     
     global model
     
-    model = load('./modelo_entrenado.pkl')    
+    model = load('../modelo_entrenado.pkl')    
     return model
-
-def load_keras_model():
-    print("cargando modelo keras..")
-    global modelo_keras
-    model = load_model('../modelo_CNN.h5')
-    model._make_predict_function()
-    print("Modelo cargado")
 
 def prepare_text(text):
     #preparo el texto para correrlo en le modelo
     texto = [text]
-    vectorizer = load('./vectorizer.pkl')
+    vectorizer = load('../vectorizer.pkl')
     new_data = vectorizer.transform(texto)
     
         # Retornar texto vectorizado
     return new_data
+
+#CNN model
+def load_keras_model():
+    print("cargando modelo keras..")
+    global modelo_keras
+    modelo_keras = load_model('../modelo_CNN.h5')
+    modelo_keras._make_predict_function()
+    print("Modelo cargado")
+
+def prepare_text_keras(text):
+    #preparo el texto para correrlo en le modelo
+    tokenizer = load('../tokenizerCNN.pkl')
+    instance = tokenizer.texts_to_sequences(text)
+    flat_list = []
+    for sublist in instance:
+        for item in sublist:
+            flat_list.append(item)
+
+    flat_list = [flat_list]
+    
+    # Retornar texto vectorizado
+    return flat_list
 
 
 @app.route("/slack_challenge", methods=["POST"])
@@ -88,7 +103,7 @@ def predict_en():
       texto = flask.request.form["text"]
       print(texto)
       
-      new_data = prepare_text(texto)
+      new_data = prepare_text_keras(texto)
 
       # Usar el modelo para predecir            
       result = model.predict(new_data)
@@ -110,6 +125,7 @@ def predict_en():
 if __name__ == "__main__":
     print("Inicializando servidor")
     load_trained_model()
+    load_keras_model()
     app.run(host='0.0.0.0', port=8080)
 
 
